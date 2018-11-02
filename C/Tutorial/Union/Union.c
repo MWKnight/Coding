87ff54ba7881c84df8f0c64d348d3e5c191e7570
Markus Wahlsten <Markus.Wahlsten@knightec.se>
2018-11-02 09:27:07 +0100

    Added C tutorial, i.e union and inputoutput

---
+++ C/Tutorial/Union/Union.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+union Data {
+	
+	int i;
+	float f;
+	char str[20];
+
+};
+
+int main() {
+	
+	union Data data;
+
+	data.i = 10;
+	printf("data.i : %d\n", data.i);
+	
+	data.f = 193.2;
+	printf("data.f : %f\n", data.f);
+	
+	strcpy( data.str, "Unions");
+	printf("data.str : %s\n", data.str);
+
+	return 0;
+}
